c6704e6a5f0a1e310a2473d32090ac3eb9bbf3eb
EliRoCas <elirocas946@gmail.com>
2024-07-20 12:38:55 -0500

    Controller changed and tested

--- models/user_roles_assignment.php
+++ models/user_roles_assignment.php
@@ -9,10 +9,10 @@ class UserRoleAssignment
     }
 
     // Método para asignar un rol a un usuario
-    public function assignUserRole($id_user, $roleId)
+    public function assignUserRole($id_user, $id_role)
     {
-        $assign = "INSERT INTO user_roles_assignments (fo_user, fo_user_role) VALUES (?, ?)";
-        $stmt = $this->connection->prepare($assign);
+        $assignSql = "INSERT INTO user_roles_assignments (fo_user, fo_user_role) VALUES (?, ?)";
+        $stmt = $this->connection->prepare($assignSql);
 
         if ($stmt === false) {
             return [
@@ -21,7 +21,7 @@ class UserRoleAssignment
             ];
         }
 
-        $stmt->bind_param("ii", $id_user, $roleId);
+        $stmt->bind_param("ii", $id_user, $id_role);
         $result = $stmt->execute();
 
         if ($result === false) {
@@ -40,10 +40,10 @@ class UserRoleAssignment
     // Método para consultar los roles de un usuario
     public function getUserRoles($id_user)
     {
-        $consult = "SELECT ur.* FROM user_roles ur
+        $getUserRolesSql = "SELECT ur.* FROM user_roles ur
                     INNER JOIN user_roles_assignments ura ON ur.id_user_role = ura.fo_user_role
                     WHERE ura.fo_user = ?";
-        $stmt = $this->connection->prepare($consult);
+        $stmt = $this->connection->prepare($getUserRolesSql);
 
         if ($stmt === false) {
             return [
@@ -67,8 +67,8 @@ class UserRoleAssignment
     // Método para eliminar un rol asignado a un usuario
     public function unassignUserRole($id_user, $roleId)
     {
-        $delete = "DELETE FROM user_roles_assignments WHERE fo_user = ? AND fo_user_role = ?";
-        $stmt = $this->connection->prepare($delete);
+        $deleteSql = "DELETE FROM user_roles_assignments WHERE fo_user = ? AND fo_user_role = ?";
+        $stmt = $this->connection->prepare($deleteSql);
 
         if ($stmt === false) {
             return [

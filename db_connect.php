902038d72e63fabe889c41245813090f4bf441a4
Eli RoCas <elir_94@hotmail.com>
2024-09-18 13:38:10 -0500

    update: DB with cdmon

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
-require 'vendor/autoload.php'; 
+require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__); 
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
@@ -13,7 +13,5 @@ $dbname = $_ENV['DB_NAME'];
 
 $connection = mysqli_connect($host, $username, $password) or die("No se encontró el servidor");
 mysqli_select_db($connection, $dbname) or die("No se encontró la Base de Datos");
-mysqli_set_charset($connection,"utf8");
+mysqli_set_charset($connection, "utf8");
 // echo "La conexión ha sido exitosa. ";
-
-?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Incluir el archivo de conexión a la base de datos
+require 'db_connect.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Validación de Aplicación</title>
+</head>
+<body>
+    <h1>¡Tu aplicación está funcionando correctamente!</h1>
+</body>
+</html>
